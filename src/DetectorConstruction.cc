0de2de25ee5c7fa510d90c397aecd24b423a0ce9
hhanif <hamza.hanif@cern.ch>
2026-04-18 13:07:48 -0700

    Initial commit (ignore build directory)

---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,197 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file DetectorConstruction.cc
+/// \brief Implementation of the DetectorConstruction class
+
+#include "DetectorConstruction.hh"
+
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4PVReplica.hh"
+#include "G4GlobalMagFieldMessenger.hh"
+#include "G4AutoDelete.hh"
+
+#include "G4GeometryManager.hh"
+#include "G4PhysicalVolumeStore.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4SolidStore.hh"
+
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+
+#include "G4PhysicalConstants.hh"
+#include "G4SystemOfUnits.hh"
+
+namespace B4
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4ThreadLocal
+G4GlobalMagFieldMessenger* DetectorConstruction::fMagFieldMessenger = nullptr;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  // Define materials
+  DefineMaterials();
+
+  // Define volumes
+  return DefineVolumes();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::DefineMaterials()
+{
+  // Lead material defined using NIST Manager
+  auto nistManager = G4NistManager::Instance();
+  nistManager->FindOrBuildMaterial("G4_PbWO4");//PbWO4
+  nistManager->FindOrBuildMaterial("G4_BARIUM_FLUORIDE");//BaF2
+  G4Element* elPb = new G4Element("Lead", "Pb", 82, 207.2*g/mole);
+  G4Element* elF = new G4Element("Fluorine", "F", 9, 18.998*g/mole);
+
+  // Liquid argon material
+  G4double a;  // mass of a mole;
+  G4double z;  // z=mean number of protons;
+  G4double density;
+  G4int numberOfatoms, nComp;
+  G4Material *PbF2 = new G4Material("LeadFluoride", density = 8.445 * g / cm3, nComp = 2);
+  PbF2->AddElement(elPb, numberOfatoms = 1);
+  PbF2->AddElement(elF, numberOfatoms = 2);
+ 
+  // Vacuum
+  new G4Material("Galactic", z=1., a=1.01*g/mole,density= universe_mean_density,
+                  kStateGas, 2.73*kelvin, 3.e-18*pascal);
+
+  // Print materials
+  G4cout << *(G4Material::GetMaterialTable()) << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::DefineVolumes()
+{
+  // Geometry parameters
+  G4double cellSizeXY = 40*mm; //25*mm -> 26*mm & 24*mm;   4cm -> 20 cm
+  G4double cellSizeZ  = 100*mm; //60.*mm -> 61*mm & 59*mm;   10cm -> 50 cm 
+  G4double nCellsXY = 5;
+  G4double nCellsZ  = 5;
+  G4double calorSizeXY = nCellsXY * cellSizeXY;
+  G4double calorSizeZ  = nCellsZ  * cellSizeZ;
+  auto worldSizeXY = calorSizeXY;
+  auto worldSizeZ  = calorSizeZ * 1; 
+
+  // Get materials
+  auto defaultMaterial = G4Material::GetMaterial("Galactic");
+  auto detectorMaterial = G4Material::GetMaterial("LeadFluoride");
+
+  if ( ! defaultMaterial || ! detectorMaterial ) {
+    G4ExceptionDescription msg;
+    msg << "Cannot retrieve materials already defined.";
+    G4Exception("DetectorConstruction::DefineVolumes()",
+      "MyCode0001", FatalException, msg);
+  }
+
+  //
+  // World
+  //
+  auto worldS = new G4Box("World", worldSizeXY/2, worldSizeXY/2, worldSizeZ/2);
+
+  auto worldLV = new G4LogicalVolume(worldS,defaultMaterial,"World");
+
+  auto worldPV = new G4PVPlacement(
+    0,                                // no rotation 
+    G4ThreeVector(0.,0.,0.),          // at (0,0,0)
+    worldLV,                          // its logical volume
+    "World",                          // its name
+    0,                                // its mother  volume
+    false,                            // no boolean operation
+    0,                                // copy number
+    false                             // checking overlaps
+  );
+
+
+  auto solidDetector=new G4Box("solidDetector", cellSizeXY/2*mm, cellSizeXY/2*mm, cellSizeZ/2*mm);
+  auto logicDetector= new G4LogicalVolume(solidDetector,detectorMaterial,"logicDetector");
+  G4double a=0;
+  for (G4int k = 0; k < nCellsZ; k++) {
+  	for (G4int j = 0; j < nCellsXY; j++) { 
+      for (G4int i = 0; i < nCellsXY; i++) { 
+        auto physDetector = new G4PVPlacement(
+          0,
+          G4ThreeVector(
+            -((calorSizeXY-cellSizeXY)/2.)*mm + (cellSizeXY*i)*mm,
+            -((calorSizeXY-cellSizeXY)/2.)*mm + (cellSizeXY*j)*mm,
+            -((calorSizeZ -cellSizeZ) /2.)*mm + (cellSizeZ *k)*mm
+          ),
+          logicDetector,
+          "physDetector",
+          worldLV,
+          false,
+          a,
+          false
+        );
+        a=a+1;
+  		}
+    }
+  }
+  //
+  // Visualization attributes
+  //
+  worldLV->SetVisAttributes (G4VisAttributes::GetInvisible());
+  auto simpleBoxVisAtt= new G4VisAttributes(G4Colour(1.0,1.0,1.0));
+  simpleBoxVisAtt->SetVisibility(true);
+  //
+  // Always return the physical World
+  //
+  return worldPV;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void DetectorConstruction::ConstructSDandField()
+{
+  // Create global magnetic field messenger.
+  // Uniform magnetic field is then created automatically if
+  // the field value is not zero.
+  G4ThreeVector fieldValue;
+  fMagFieldMessenger = new G4GlobalMagFieldMessenger(fieldValue);
+  fMagFieldMessenger->SetVerboseLevel(1);
+
+  // Register the field messenger for deleting
+  G4AutoDelete::Register(fMagFieldMessenger);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+} // end namespace
---
+++ src/SteppingAction.cc
@@ -0,0 +1,90 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B4/B4a/src/SteppingAction.cc
+/// \brief Implementation of the B4a::SteppingAction class
+
+#include "SteppingAction.hh"
+#include "EventAction.hh"
+#include "DetectorConstruction.hh"
+
+#include "G4Step.hh"
+#include "G4RunManager.hh"
+#include "G4AnalysisManager.hh"
+
+using namespace B4;
+
+namespace B4a
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+SteppingAction::SteppingAction(const DetectorConstruction* detConstruction,
+                               EventAction* eventAction)
+  : fDetConstruction(detConstruction),
+    fEventAction(eventAction)
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void SteppingAction::UserSteppingAction(const G4Step* step)
+{
+// Collect energy and track length step by step
+
+  // get volume of the current step
+  auto volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume();
+
+  // energy deposit
+  auto edep = step->GetTotalEnergyDeposit();
+
+  // Step coordinates
+  auto x = step->GetPostStepPoint()->GetPosition().x();
+  auto y = step->GetPostStepPoint()->GetPosition().y();
+  auto z = step->GetPostStepPoint()->GetPosition().z();
+
+  // get event number
+  auto eventID = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
+
+  // get gun energy
+  auto gunEnergy = G4RunManager::GetRunManager()->GetCurrentEvent()->GetPrimaryVertex(0)->GetPrimary()->GetKineticEnergy();
+
+  // get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // fill ntuple with event ID too
+  analysisManager->FillNtupleIColumn(0, eventID);
+  analysisManager->FillNtupleDColumn(1, gunEnergy);
+  analysisManager->FillNtupleDColumn(2, x);
+  analysisManager->FillNtupleDColumn(3, y);
+  analysisManager->FillNtupleDColumn(4, z);
+  analysisManager->FillNtupleDColumn(5, edep);
+  analysisManager->AddNtupleRow();
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}
